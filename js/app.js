948e8ebe20910d69c668a318e30401084be2ce27
harry-duncan <harryduncan25@gmail.com>
2015-11-23 16:13:51 +1100

    savepoint 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-// Your app must:
+// gameboard in html
 
-// Render a game board in the browser
-// Switch turns between X and O (or whichever markers you select)
-// Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
+// [1][2][3]
+// [4][5][6]
+// [7][8][9]
+
+
+// Need to be able to choose a player // X & O
+// Need to be able to switch turns 
+
+// Need to record score // win - loss - draw and tally
+// need to have a grid board and a win system
+
+// need to have players and choose a piece 
+
+// need to have listeners for all of these 
+
+// need to ensue the use of jQuery
+
+// need to go have a kit kat and have a break 
+
+var player1 = 'O';
+var player2 = 'X';
+
 
