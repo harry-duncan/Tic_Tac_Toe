370875ce91145d46edeffe09fb2f0fe29cf8a605
harry-duncan <harryduncan25@gmail.com>
2015-11-23 15:55:16 +1100

    first savepoint

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Your app must:
+
+// Render a game board in the browser
+// Switch turns between X and O (or whichever markers you select)
+// Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
+
